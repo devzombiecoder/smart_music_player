f19da05fff7f67ff82ce29608d74b06629a20843
sameer choudhary <95902961+IAMsameerChoudhary@users.noreply.github.com>
2021-12-11 14:34:40 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,55 @@
+let allMusic = [{
+        name: "1",
+        artist: "Jordan Schor",
+        img: "1",
+        src: "bechara"
+    },
+    {
+        name: "2",
+        artist: "Audio Library",
+        img: "2",
+        src: "dil"
+    },
+    {
+        name: "3",
+        artist: "Beauz & Jvna",
+        img: "3",
+        src: "ghar"
+    },
+    {
+        name: "4",
+        artist: "Mike Archangelo",
+        img: "4",
+        src: "jab"
+    },
+    {
+        name: "5",
+        artist: "Jim Yosef x Roy",
+        img: "5",
+        src: "Kal"
+    },
+    {
+        name: "6",
+        artist: "NCS",
+        img: "6",
+        src: "mera"
+    },
+    {
+        name: "7",
+        artist: "Mike Archangelo",
+        img: "7",
+        src: "mere"
+    },
+    {
+        name: "8",
+        artist: "Jim Yosef x Roy",
+        img: "8",
+        src: "pardesi"
+    },
+    {
+        name: "9",
+        artist: "NCS Release",
+        img: "9",
+        src: "pehli"
+    }
+];
